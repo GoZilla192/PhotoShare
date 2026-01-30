24df9a15be0602209fc6a88e90daef5945ef2947
NeFoRMaLiK-pixel <fikyc55@gmail.com>
2026-01-30 15:15:04 +0200

    Convert user repository to class

--- app/repository/users.py
+++ app/repository/users.py
@@ -1,13 +1,27 @@
+from fastapi import Depends
 from sqlalchemy import select, func
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from database.db import get_async_session
 from models.user import User
 
-async def count_users(session: AsyncSession) -> int:
-    res = await session.execute(select(func.count(User.id)))
-    return res.scalar_one()
 
-async def create_user(session: AsyncSession, user: User) -> User:
-    session.add(user)
-    await session.commit()
-    await session.refresh(user)
-    return user
+class UserRepository:
+    def __init__(self, session: AsyncSession) -> None:
+        self._session = session
+
+    async def count_users(self) -> int:
+        res = await self._session.execute(select(func.count(User.id)))
+        return res.scalar_one()
+
+    async def create_user(self, user: User) -> User:
+        self._session.add(user)
+        await self._session.commit()
+        await self._session.refresh(user)
+        return user
+
+
+def get_user_repository(
+    session: AsyncSession = Depends(get_async_session),
+) -> UserRepository:
+    return UserRepository(session)
