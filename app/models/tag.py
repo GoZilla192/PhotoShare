115e4a519e651ec492660ae45f80f87160a570c9
vol0dumur <160355905+vol0dumur@users.noreply.github.com>
2026-01-31 21:40:46 +0200

    Update tag.py

--- app/models/tag.py
+++ app/models/tag.py
@@ -1,17 +1,25 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
+from __future__ import annotations
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database import Base
 from app.models.photo_tags import photo_tags
+from app.models import Photo
 
 
 class Tag(Base):
     __tablename__ = "tags"
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(100), unique=True, nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
 
-    photos = relationship(
+    name: Mapped[str] = mapped_column(
+        String(100),
+        unique=True,
+        nullable=False
+    )
+
+    # many-to-many
+    photos: Mapped[list["Photo"]] = relationship(
         "Photo",
         secondary=photo_tags,
         back_populates="tags"
