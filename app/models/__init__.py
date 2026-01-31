14885d5be1d4a68621b9a67c176698d6ce886bd3
vol0dumur <160355905+vol0dumur@users.noreply.github.com>
2026-01-31 21:38:14 +0200

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,10 +1,6 @@
-from .user import User
-from .photo import Photo
-from .tag import Tag
-from .comment import Comment
-from .transformed_image import TransformedImage
-from .public_link import PublicLink
-
-# additional functionality. just in case we have time
-# from .rating import Rating
-# from .token_blacklist import TokenBlacklist
+from app.models.user import User
+from app.models.photo import Photo
+from app.models.tag import Tag
+from app.models.comment import Comment
+from app.models.transformed_image import TransformedImage
+from app.models.public_link import PublicLink
