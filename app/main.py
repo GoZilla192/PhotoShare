3c93e4280e795f85c408e24dfd64187b9cff4aae
vol0dumur <160355905+vol0dumur@users.noreply.github.com>
2026-02-05 15:51:51 +0200

    Added router.

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
+
 from app.routers.auth import router as auth_router
-
 from app.routers.photos import router as photos_router
+from app.routers.qr import qr_router
+from app.routers.tags import tags_router
 
-from app.routers import tags
 
 app = FastAPI()
 
-app.include_router(tags.router)
-app.include_router(photos_router)
 app.include_router(auth_router)
+app.include_router(photos_router)
+app.include_router(qr_router)
+app.include_router(tags_router)
