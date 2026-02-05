805413c3aa4a0bd15d483daca09176c874dbe1ee
NeFoRMaLiK-pixel <fikyc55@gmail.com>
2026-02-05 15:41:12 +0200

    Add admin photo route with role guard

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from app.routers.auth import router as auth_router
 
+from app.routers.admin import router as admin_router
 from app.routers.photos import router as photos_router
 
 
 app = FastAPI()
+app.include_router(admin_router)
 app.include_router(photos_router)
 app.include_router(auth_router)
