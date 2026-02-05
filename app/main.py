920c93eeab5bcd3b157ce777ec85fb0af4fc48d9
RAtnik00 <yaremenkodmitriy22@gmail.com>
2026-02-05 15:04:53 +0100

    Correct active user dependency for comment routes

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from app.routers.auth import router as auth_router
-
 from app.routers.photos import router as photos_router
+from app.routers.comments import router as comments_router
 
 
 app = FastAPI()
 app.include_router(photos_router)
 app.include_router(auth_router)
+app.include_router(comments_router)
